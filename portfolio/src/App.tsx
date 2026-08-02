b786e2252ec7d1c87d156753c7440ce29b518902
muhanif-dev <muhanif.dev@gmail.com>
2026-08-02 13:04:42 +0500

    build: integrate Tailwind CSS with Vite

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <h1>Portfoio hey</h1>
+    <h1 className="text-orange-500">Portfoio hey</h1>
   )
 }
 
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 //This imports the API responsible for rendering your React application into the browser.
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import "./index.css";
 
 // (!). This is the non-null assertion operator in TypeScript.
 createRoot(document.getElementById('root')!).render(
